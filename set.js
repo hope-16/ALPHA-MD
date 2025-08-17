92373c2193b29ac7a554577fe010286484f174bb
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-08-17 08:04:39 +0300

    @keithkeizzah

--- set.js
+++ set.js
@@ -21,7 +21,7 @@ const config = {
     session: process.env.SESSION_ID || '', //paste your session here 
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "Keith", //replace with your owner name
-    NUMERO_OWNER: process.env.NUMERO_OWNER || "254748387615",   //replace with your owner number  
+    NUMERO_OWNER: process.env.OWNER_NUMBER || "254748387615",   //replace with your owner number  
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',    
     URL: process.env.URL || "https://files.catbox.moe/4h8lfw.jpg",    //replace with your image url                     
     ANTICALL_MSG: process.env.ANTICALL_MSG || 'call declined',             
